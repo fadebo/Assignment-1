65337a2c9b385b5138f6b35d5dc68b12c15a1ebd
Farrell Adebo <168660530+fadebo@users.noreply.github.com>
2024-05-15 23:29:41 +0000

    Updates the index page to link to the login page and updates the login page with a form action. - Not Functional

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
   <body>
     <h1> Assignment 1</h1>
     <p>Welcome <?php echo $name?>, You successfully logged in at <?php echo $date?> </p>
+    <p><a href="/login.php">Click here to login</a></p>
   </body>
 </html>
--- login.php
+++ login.php
@@ -8,13 +8,18 @@
     <title>Login Page</title>
   </head>
   <body>
-      <form> 
+      <form action="/validate.php" method="post"> 
           <div class= "form-group">
               <label for="username">Username</label>
+              <br>
               <input type="text" class="form-control" id="username" placeholder="Enter username">
+              <br>
               <label for="password">Password</label>
+              <br>
               <input type="password" class="form-control" id="password" placeholder="Enter password">
+              <br>
               <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
       </form>
   </body>
 </html>
